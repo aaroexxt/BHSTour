8f9c1df7358f0312e65b2790490822abdf8361a5
Aaron Becker <aaron.becker@gmail.com>
2018-10-19 13:16:31 -0700

    1st class commit

---
+++ main.js
@@ -0,0 +1,41 @@
+/*
+
+BHS Virtual Tour Application
+
+For Architecture Class, Period 5
+Copyright Aaron Becker
+
+
+WIN COMMANDS:
+
+net use X: \\bhs-nad\home\students\21BeckerA00
+cd X:\BHSTour\
+*/
+
+
+console.log("Aaron's Tour Application - init begin");
+
+console.log("1/3: Requiring stuff");
+const fs = require('fs');
+const express = require('express');
+
+console.log("2/3: Reading configs");
+try {
+	var configData = fs.readFileSync("config.json", "utf8");
+	configData = JSON.parse(configData);
+	console.log("configData: "+JSON.stringify(configData));
+} catch(e) {
+	throw "Error reading config: "+e;
+}
+console.log("3/3: Initializing routes and express");
+const app = express();
+console.log("routes init")
+app.get('/', (req, res) => res.send('Hello World!'))
+
+console.log("server init");
+if (typeof configData.serverPort == "undefined") {
+	throw "Port undefined in configData";
+}
+app.listen(configData.serverPort, () => console.log(`server OK, port=${configData.serverPort}!`))
+
+console.log("init end")
